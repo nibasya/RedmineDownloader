d535df41bd36f9b211148ae6e198bc33397d1937
donadona <15785731+nibasya@users.noreply.github.com>
2026-03-15 12:55:11 +0900

    intellisenseがプロジェクトの_MSC_VERを認識しないせいで_MSC_VERが1950以上の環境でintellisenseが大量のエラーを報告する問題に対処

--- RedmineDownloader/pch.h
+++ RedmineDownloader/pch.h
@@ -1,4 +1,4 @@
-﻿// pch.h: プリコンパイル済みヘッダー ファイルです。  
+// pch.h: プリコンパイル済みヘッダー ファイルです。  
 // 次のファイルは、その後のビルドのビルド パフォーマンスを向上させるため 1 回だけコンパイルされます。  
 // コード補完や多くのコード参照機能などの IntelliSense パフォーマンスにも影響します。  
 // ただし、ここに一覧表示されているファイルは、ビルド間でいずれかが更新されると、すべてが再コンパイルされます。  
@@ -13,9 +13,11 @@
 #if defined(_MSC_VER) && (_MSC_VER < 1950)
 	#pragma warning(push)
 	#pragma warning(disable:4996)
-	#include <cpprest/json.h>
-	#include <cpprest/filestream.h>
-	#include <cpprest/http_client.h>
+#endif // _MSC_VER < 1950
+#include <cpprest/json.h>
+#include <cpprest/filestream.h>
+#include <cpprest/http_client.h>
+#if defined(_MSC_VER) && (_MSC_VER < 1950)
 	#pragma warning(pop)
 #endif // _MSC_VER < 1950
 
