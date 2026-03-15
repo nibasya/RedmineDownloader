c29cfb4f6256a8e6bef6b8a41c9fb24441b93adc
donadona <15785731+nibasya@users.noreply.github.com>
2026-03-15 13:43:49 +0900

    downloader、viewer共通項目をShared/CommonConfig.hへ移動

---
+++ Shared/CommonConfig.h
@@ -0,0 +1,9 @@
+#pragma once
+
+const CString IssueListFileName(_T("\\issue_list.json"));
+const CString IssueFileName(_T("\\_issue.json"));
+const CString RedmineDataFolder(_T("\\RedmineData"));
+const CString MembershipsFileName(_T("\\memberships.json"));
+const CString IssueStatusesFileName(_T("\\issue_statuses.json"));
+const CString TrackersFileName(_T("\\trackers.json"));
+const CString IssuePrioritiesFileName(_T("\\issue_priorities.json"));
